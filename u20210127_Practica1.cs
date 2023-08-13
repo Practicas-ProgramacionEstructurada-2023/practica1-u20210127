07865a7fd33e9d250ac85c675c714ceac06dbfb1
u20210127 <u20210127@univo.edu.sv>
2023-08-13 15:03:04 -0600

    Practica1

--- u20210127_Practica1.cs
+++ u20210127_Practica1.cs
@@ -7,6 +7,10 @@ namespace MyApp// Note: actual namespace depends on the project name.
          static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world");
         }
     }
 }
